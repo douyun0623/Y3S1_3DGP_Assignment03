83770369eb49074df9ba399f239d1b8b52b3c140
douyun0623 <dyk6610@naver.com>
2025-06-22 14:42:34 +0900

    terrain 위에 오브젝트 지우기

--- Scene.cpp
+++ Scene.cpp
@@ -83,10 +83,10 @@ void CScene::BuildObjects(ID3D12Device* pd3dDevice, ID3D12GraphicsCommandList* p
 		257, xmf3Scale, xmf4Color);
 #endif
 
-	m_nShaders = 1;
+	/*m_nShaders = 1;
 	m_pShaders = new CObjectsShader[m_nShaders];
 	m_pShaders[0].CreateShader(pd3dDevice, m_pd3dGraphicsRootSignature);
-	m_pShaders[0].BuildObjects(pd3dDevice, pd3dCommandList, m_pTerrain);
+	m_pShaders[0].BuildObjects(pd3dDevice, pd3dCommandList, m_pTerrain);*/
 
 }
 
